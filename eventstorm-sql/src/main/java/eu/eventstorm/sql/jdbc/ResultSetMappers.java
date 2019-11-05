6b1b3a96d0fc891c3ec315b79979cd75c274df60
Jacques Militello <Jacques Militello@DESKTOP-VIBH6GO>
2019-11-05 21:22:00 +0100

    add tests

--- eventstorm-sql/src/main/java/eu/eventstorm/sql/jdbc/ResultSetMappers.java
+++ eventstorm-sql/src/main/java/eu/eventstorm/sql/jdbc/ResultSetMappers.java
@@ -1,24 +1,43 @@
 package eu.eventstorm.sql.jdbc;
 
+import static com.google.common.collect.ImmutableMap.of;
+
+import com.google.common.collect.ImmutableMap;
+
+import eu.eventstorm.sql.EventstormSqlException;
+import eu.eventstorm.sql.EventstormSqlExceptionType;
+
 /**
  * @author <a href="mailto:jacques.militello@gmail.com">Jacques Militello</a>
  */
 public final class ResultSetMappers {
 
-    public static final ResultSetMapper<Long> SINGLE_LONG = (dialect, rs) -> {
-        long value = rs.getLong(1);
-        if (rs.next()) {
-            throw new ResultSetMapperException("more than one result");
-        }
-        return value;
-    };
+	public static final ResultSetMapper<Long> SINGLE_LONG = (dialect, rs) -> {
+		long value = rs.getLong(1);
+		if (rs.next()) {
+			throw new ResultSetMapperException(ResultSetMapperException.Type.MORE_THAN_ONE_RESULT, of("value", value));
+		}
+		return value;
+	};
 
-    public static final ResultSetMapper<String> STRING = (dialect, rs) -> {
-        return rs.getString(1);
-    };
+	public static final ResultSetMapper<String> STRING = (dialect, rs) -> {
+		return rs.getString(1);
+	};
 
-    private ResultSetMappers() {
-    }
+	private ResultSetMappers() {
+	}
 
+	@SuppressWarnings("serial")
+	public static class ResultSetMapperException extends EventstormSqlException {
+
+		public enum Type implements EventstormSqlExceptionType {
+			MORE_THAN_ONE_RESULT
+		}
+
+		public ResultSetMapperException(Type type, ImmutableMap<String, Object> params) {
+			super(type, params);
+		}
+
+	}
 
 }
---
+++ eventstorm-sql/src/test/java/eu/eventstorm/sql/jdbc/ResultSetMappersTest.java
@@ -0,0 +1,50 @@
+package eu.eventstorm.sql.jdbc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+
+import eu.eventstorm.sql.Dialect;
+
+/**
+ * @author <a href="mailto:jacques.militello@gmail.com">Jacques Militello</a>
+ */
+class ResultSetMappersTest {
+
+	@Test
+	void singleLongTest() throws SQLException {
+
+		Dialect dialect = mock(Dialect.class);
+		ResultSet rs = mock(ResultSet.class);
+
+		when(rs.getLong(1)).thenReturn(123456L);
+		when(rs.next()).thenReturn(false);
+		assertEquals(123456L, ResultSetMappers.SINGLE_LONG.map(dialect, rs));
+
+		reset(rs);
+		when(rs.getLong(1)).thenReturn(123456L);
+		when(rs.next()).thenReturn(true);
+		ResultSetMappers.ResultSetMapperException ex = assertThrows(ResultSetMappers.ResultSetMapperException.class, () -> ResultSetMappers.SINGLE_LONG.map(dialect, rs));
+		
+		assertEquals(ResultSetMappers.ResultSetMapperException.Type.MORE_THAN_ONE_RESULT, ex.getType());
+	}
+	
+	@Test
+	void stringTest() throws SQLException {
+
+		Dialect dialect = mock(Dialect.class);
+		ResultSet rs = mock(ResultSet.class);
+
+		when(rs.getString(1)).thenReturn("hello world");
+
+		assertEquals("hello world", ResultSetMappers.STRING.map(dialect, rs));
+	}
+
+}
