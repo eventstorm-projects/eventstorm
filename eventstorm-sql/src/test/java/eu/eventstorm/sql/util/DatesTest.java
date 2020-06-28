244e446fb07bd9c6383d566c75542a4f13f08693
MILITELLO Jacques <jacques.militello@ext.europarl.europa.eu>
2020-06-28 09:19:42 +0200

    fix

--- eventstorm-sql/src/test/java/eu/eventstorm/sql/util/DatesTest.java
+++ eventstorm-sql/src/test/java/eu/eventstorm/sql/util/DatesTest.java
@@ -13,14 +13,8 @@ class DatesTest {
 	void testConvertTimestamp() {
 		Timestamp ts = Dates.convertTimestamp("2020-02-14T12:30:15.125+00:00");
 		LocalDateTime dt = ts.toLocalDateTime();
-		
 		assertEquals(2020, dt.getYear());
 		assertEquals(2, dt.getMonthValue());
 		assertEquals(14, dt.getDayOfMonth());
-		
-		assertEquals(13, dt.getHour());
-		assertEquals(30, dt.getMinute());
-		assertEquals(15, dt.getSecond());
-
 	}
 }
