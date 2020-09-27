1b85db1a31c090f6bd56a56122f839271bfc1080
Jacques Militello <jacques.militello@gmail.com>
2020-09-27 18:05:11 +0200

    add getCode for i18n

--- eventstorm-core-api/src/main/java/eu/eventstorm/core/EventstormExceptionType.java
+++ eventstorm-core-api/src/main/java/eu/eventstorm/core/EventstormExceptionType.java
@@ -1,8 +1,17 @@
 package eu.eventstorm.core;
 
+import eu.eventstorm.util.Strings;
+
 /**
  * @author <a href="mailto:jacques.militello@gmail.com">Jacques Militello</a>
  */
 public interface EventstormExceptionType {
 
+	/**
+	 * The code for this error (to manage i18n errors) 
+	 */
+	default String getCode() {
+		return Strings.EMPTY;
+	}
+	
 }
