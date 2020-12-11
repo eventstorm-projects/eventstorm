c58947b53a97f3040dc5291d5ff05302aa36b12f
Jacques Militello <jacques.militello@gmail.com>
2020-12-11 23:43:57 +0100

    simplify dispatch

--- eventstorm-cqrs/src/main/java/eu/eventstorm/cqrs/CommandGateway.java
+++ eventstorm-cqrs/src/main/java/eu/eventstorm/cqrs/CommandGateway.java
@@ -1,10 +1,8 @@
 package eu.eventstorm.cqrs;
 
-import static com.google.common.collect.ImmutableMap.of;
+import com.google.common.collect.ImmutableMap;
 
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import reactor.util.function.Tuples;
 
 /**
  * @author <a href="mailto:jacques.militello@gmail.com">Jacques Militello</a>
@@ -17,18 +15,13 @@ public final class CommandGateway {
         this.registry = registry;
     }
 
-	public <T extends Command, E> Flux<E> dispatch(CommandContext ctx, T cmd) {
-
-		return Mono.just(Tuples.of(ctx, cmd))
-				// retrieve command handler associated
-				.flatMapMany(tuple -> {
-					CommandHandler<T,E> handler = registry.<T,E>get(tuple.getT2());
-					// if no command handler => error
-					if (handler == null) {
-						throw new CommandGatewayException(CommandGatewayException.Type.NOT_FOUND, of("command", tuple.getT2()));
-					}
-					return handler.handle(tuple.getT1(), tuple.getT2());
-				});
+	public <T extends Command, E> Flux<E> dispatch(CommandContext ctx, T command) {
+		CommandHandler<T,E> handler = registry.<T,E>get(command);
+		// if no command handler => error
+		if (handler == null) {
+			throw new CommandGatewayException(CommandGatewayException.Type.NOT_FOUND, ImmutableMap.of("command", command));
+		}
+		return handler.handle(ctx, command);
     }
 
 }
