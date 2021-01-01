8b173452ee302fbe1dd446b445c46027c0fbe4a0
Jacques Militello <jacques.militello@gmail.com>
2021-01-02 00:10:23 +0100

    fix

--- eventstorm-sql-spring/src/test/java/eu/eventstorm/sql/spring/ex001/Module.java
+++ eventstorm-sql-spring/src/test/java/eu/eventstorm/sql/spring/ex001/Module.java
@@ -1,8 +1,6 @@
 package eu.eventstorm.sql.spring.ex001;
 
-import javax.annotation.Generated;
-
-@Generated("eu.eventsotrm.sql.apt.ModuleGenerator")
+//@Generated("eu.eventsotrm.sql.apt.ModuleGenerator")
 public final class Module extends eu.eventstorm.sql.Module { 
 
      public Module(String name, String catalog) {
