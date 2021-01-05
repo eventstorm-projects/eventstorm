dd4415f83aa6d42f71af9a7669bdd16f92ea468c
Jacques Militello <jacques.militello@gmail.com>
2021-01-05 16:27:02 +0100

    clean

--- eventstorm-sql/src/main/java/eu/eventstorm/sql/impl/TransactionException.java
+++ eventstorm-sql/src/main/java/eu/eventstorm/sql/impl/TransactionException.java
@@ -4,8 +4,6 @@ import java.sql.SQLException;
 
 import eu.eventstorm.sql.EventstormSqlException;
 import eu.eventstorm.sql.EventstormSqlExceptionType;
-import eu.eventstorm.sql.Transaction;
-import eu.eventstorm.sql.tracer.TransactionSpan;
 
 /**
  * @author <a href="mailto:jacques.militello@gmail.com">Jacques Militello</a>
@@ -13,14 +11,13 @@ import eu.eventstorm.sql.tracer.TransactionSpan;
 public final class TransactionException extends EventstormSqlException {
 
     /**
-	 *  Generated seruql UID.
+	 *  Generated serial UID.
 	 */
 	private static final long serialVersionUID = -7353728745195001854L;
 
 	public enum Type implements EventstormSqlExceptionType {
 		PREPARED_STATEMENT, CREATE, NO_CURRENT_TRANSACTION,  READ_ONLY, CONNECTION_ISOLATION, NOT_ACTIVE, COMMIT , ROLLBACK
     }
-
 
     public TransactionException(Type type) {
         super(type, null);
