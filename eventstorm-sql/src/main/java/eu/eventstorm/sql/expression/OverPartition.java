d75e1ee52430f40e3de9b2cdad892e6d7e017e19
Jacques Militello <jacques.militello@gmail.com>
2021-01-13 21:47:43 +0100

    fix OverPartition

--- eventstorm-sql/src/main/java/eu/eventstorm/sql/expression/OverPartition.java
+++ eventstorm-sql/src/main/java/eu/eventstorm/sql/expression/OverPartition.java
@@ -6,7 +6,7 @@ import eu.eventstorm.sql.desc.DerivedColumn;
 /**
  * @author <a href="mailto:jacques.militello@gmail.com">Jacques Militello</a>
  */
-public interface OverPartition extends DerivedColumn {
+public interface OverPartition extends AggregateFunction {
 
 	 String build(Dialect dialect, boolean alias);
 	 
