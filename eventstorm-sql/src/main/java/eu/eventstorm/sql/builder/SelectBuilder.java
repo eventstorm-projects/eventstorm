09a994f21f4108f13938e46ac5cb505c43dd8304
MILITELLO Jacques <jacques.militello@ext.europarl.europa.eu>
2021-11-22 11:32:33 +0100

    add groupby for pageable

--- eventstorm-sql/src/main/java/eu/eventstorm/sql/builder/SelectBuilder.java
+++ eventstorm-sql/src/main/java/eu/eventstorm/sql/builder/SelectBuilder.java
@@ -172,6 +172,7 @@ public final class SelectBuilder extends AbstractBuilder {
 		appendSelect(builder);
 		appendFrom(builder);
 		appendWherePage(builder, pageRequest);
+        appendGroupBy(builder);
 		appendOrderPage(builder, pageRequest);
 
 		builder.append(' ').append(this.database().dialect().range(pageRequest.getOffset(), pageRequest.getSize()));
