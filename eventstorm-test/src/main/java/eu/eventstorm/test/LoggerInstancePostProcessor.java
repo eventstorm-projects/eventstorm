363d896bc5445c1db322a2c306a7c54cee4c5139
Jacques Militello <jacques.militello@gmail.com>
2021-12-14 07:39:44 +0100

    remove log4j2

--- eventstorm-test/src/main/java/eu/eventstorm/test/LoggerInstancePostProcessor.java
+++ eventstorm-test/src/main/java/eu/eventstorm/test/LoggerInstancePostProcessor.java
@@ -1,56 +1,46 @@
 package eu.eventstorm.test;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.config.LoggerConfig;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestInstancePostProcessor;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author <a href="mailto:jacques.militello@gmail.com">Jacques Militello</a>
  */
 public final class LoggerInstancePostProcessor implements TestInstancePostProcessor {
 
-	@Override
-	public void postProcessTestInstance(Object o, ExtensionContext extensionContext) throws Exception {
-		String level = System.getProperty("log4j2.level");
-		if (level != null) {
-			configLoggers(Level.valueOf(level));
-		}
+    @Override
+    public void postProcessTestInstance(Object o, ExtensionContext extensionContext) throws Exception {
+        String level = System.getProperty("logback.level");
+        if (level != null) {
+            configLoggers(Level.valueOf(level));
+        }
 
-		// The job exceeded the maximum log length, and has been terminated.
-		String travis = System.getProperty("travis");
-		if (travis != null) {
-			removeAllAppenders();
-		}
-	}
+        // The job exceeded the maximum log length, and has been terminated.
+        String travis = System.getProperty("travis");
+        if (travis != null) {
+            //removeAllAppenders();
+        }
+    }
 
-	private static void configLoggers(Level level) {
-		LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
-		Configuration config = ctx.getConfiguration();
-		LoggerConfig loggerConfig = config.getLoggerConfig(LogManager.ROOT_LOGGER_NAME);
-		if (Level.ALL == level) {
-			loggerConfig.setLevel(Level.INFO);
-			for (LoggerConfig lc : config.getLoggers().values()) {
-				if (lc.getName().startsWith("org") || lc.getName().startsWith("com") || lc.getName().startsWith("io")) {
-					lc.setLevel(Level.INFO);
-				} else {
-					lc.setLevel(level);	
-				}
-			}
-		} else {
-			loggerConfig.setLevel(level);
-			for (LoggerConfig lc : config.getLoggers().values()) {
-				lc.setLevel(level);
-			}	
-		}
-		
-		ctx.updateLoggers();
-	}
+    private static void configLoggers(Level level) {
+        if (level != null) {
+            LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+            for (Logger lc : loggerContext.getLoggerList()) {
+                if (lc.getName().startsWith("org") || lc.getName().startsWith("com") || lc.getName().startsWith("io")) {
+                    lc.setLevel(Level.INFO);
+                } else {
+                    lc.setLevel(level);
+                }
+            }
+        }
+    }
 
-	private void removeAllAppenders() {
+    private void removeAllAppenders() {
+		/*LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
 		LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
 		Configuration config = ctx.getConfiguration();
 		LoggerConfig loggerConfig = config.getLoggerConfig(LogManager.ROOT_LOGGER_NAME);
@@ -61,6 +51,7 @@ public final class LoggerInstancePostProcessor implements TestInstancePostProces
 			}
 		});
 		ctx.updateLoggers();
-	}
+		*/
+    }
 
 }
