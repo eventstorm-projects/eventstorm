a7c745308f5481e13e8e302a3a250c6684e245a5
Jacques Militello <jacques.militello@gmail.com>
2022-06-17 20:32:06 +0200

    support uuid postgres

--- eventstorm-sql/src/main/java/eu/eventstorm/sql/jdbc/PreparedStatementSetters.java
+++ eventstorm-sql/src/main/java/eu/eventstorm/sql/jdbc/PreparedStatementSetters.java
@@ -1,5 +1,7 @@
 package eu.eventstorm.sql.jdbc;
 
+import eu.eventstorm.sql.Dialect;
+
 /**
  * @author <a href="mailto:jacques.militello@gmail.com">Jacques Militello</a>
  */
@@ -16,6 +18,10 @@ public final class PreparedStatementSetters {
     
     public static PreparedStatementSetter setSingleString(String value) {
     	return ps -> ps.setString(1, value);
+    }
+
+    public static PreparedStatementSetter setSingleUuid(Dialect dialect, String value) {
+        return ps -> dialect.setPreparedStatement(ps, 1, value);
     }
     
 }
