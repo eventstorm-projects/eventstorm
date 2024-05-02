968d612a1dfbe850cf40d8e7ee6e368ed0b0b7ed
Jacques Militello <jacques.militello@gmail.com>
2024-05-02 17:02:06 +0200

    usage of UUID for Postgresql

--- eventstorm-sql-apt/src/main/java/eu/eventstorm/sql/apt/liquibase/DatabaseDialectPostgres.java
+++ eventstorm-sql-apt/src/main/java/eu/eventstorm/sql/apt/liquibase/DatabaseDialectPostgres.java
@@ -1,6 +1,7 @@
 package eu.eventstorm.sql.apt.liquibase;
 
 import eu.eventstorm.sql.annotation.Column;
+import eu.eventstorm.sql.annotation.ColumnFormat;
 import eu.eventstorm.sql.annotation.PrimaryKey;
 import eu.eventstorm.sql.apt.log.Logger;
 import eu.eventstorm.sql.type.Json;
@@ -13,122 +14,125 @@ import java.sql.Timestamp;
 
 /**
  * @author <a href="mailto:jacques.militello@gmail.com">Jacques Militello</a>
- * 
  */
 final class DatabaseDialectPostgres implements DatabaseDialect {
 
-	static final DatabaseDialect INSTANCE = new DatabaseDialectPostgres();
+    static final DatabaseDialect INSTANCE = new DatabaseDialectPostgres();
 
-	static Logger logger;
+    static Logger logger;
 
-	@Override
-	public String toSqlType(String javaType, PrimaryKey column) {
-		
+    @Override
+    public String toSqlType(String javaType, PrimaryKey column) {
 
-		if (int.class.getName().equals(javaType) || Integer.class.getName().equals(javaType)) {
-			return "INTEGER";
-		}
 
-		if (long.class.getName().equals(javaType) || Long.class.getName().equals(javaType)) {
-			return "BIGINT";
-		}
-		
-		if (String.class.getName().equals(javaType)) {
-			return "VARCHAR(" + column.length() + ")";
-		}
-		
-		logger.error("No sql type for java type (PrimaryKey) [" + javaType + "]");
-		return null;
-	}
-	
-	@Override
-	public String toSqlType(String javaType, Column column) {
+        if (int.class.getName().equals(javaType) || Integer.class.getName().equals(javaType)) {
+            return "INTEGER";
+        }
 
-		if (int.class.getName().equals(javaType) || Integer.class.getName().equals(javaType)) {
-			return "INTEGER";
-		}
+        if (long.class.getName().equals(javaType) || Long.class.getName().equals(javaType)) {
+            return "BIGINT";
+        }
 
-		if (long.class.getName().equals(javaType) || Long.class.getName().equals(javaType)) {
-			return "BIGINT";
-		}
+        if (String.class.getName().equals(javaType)) {
+            if (column.format() == ColumnFormat.UUID) {
+                return "UUID";
+            } else {
+                return "VARCHAR(" + column.length() + ")";
+            }
+        }
 
-		if (byte.class.getName().equals(javaType) || Byte.class.getName().equals(javaType)) {
-			return "SMALLINT";
-		}
-		
-		if (boolean.class.getName().equals(javaType) || Boolean.class.getName().equals(javaType)) {
-			return "BOOLEAN";
-		}
-		
-		if (String.class.getName().equals(javaType)) {
-			return "VARCHAR(" + column.length() + ")";
-		}
+        logger.error("No sql type for java type (PrimaryKey) [" + javaType + "]");
+        return null;
+    }
 
-		if (Timestamp.class.getName().equals(javaType)) {
-			return "TIMESTAMP WITH TIME ZONE";
-		}
-		
-		if (Date.class.getName().equals(javaType)) {
-			return "DATE";
-		}
-		
-		if (double.class.getName().equals(javaType) || Double.class.getName().equals(javaType)) {
-			return "DECIMAL";
-		}
+    @Override
+    public String toSqlType(String javaType, Column column) {
 
-		if (float.class.getName().equals(javaType) || Float.class.getName().equals(javaType)) {
-			return "DECIMAL";
-		}
-		
-		if (Json.class.getName().equals(javaType)) {
-			return "JSONB";
-		}
+        if (int.class.getName().equals(javaType) || Integer.class.getName().equals(javaType)) {
+            return "INTEGER";
+        }
 
-		if (Xml.class.getName().equals(javaType)) {
-			return "XML";
-		}
-		
-		if (Blob.class.getName().equals(javaType)|| "byte[]".equals(javaType)) {
-			return "BLOB";
-		}
-		
-		if (Clob.class.getName().equals(javaType)) {
-			return "CLOB";
-		}
+        if (long.class.getName().equals(javaType) || Long.class.getName().equals(javaType)) {
+            return "BIGINT";
+        }
 
-		if ("long[]".equals(javaType) || "java.lang.Long[]".equals(javaType)) {
-			return "int8[]";
-		}
+        if (byte.class.getName().equals(javaType) || Byte.class.getName().equals(javaType)) {
+            return "SMALLINT";
+        }
 
-		if ("int[]".equals(javaType) || "java.lang.Integer[]".equals(javaType)) {
-			return "int4[]";
-		}
+        if (boolean.class.getName().equals(javaType) || Boolean.class.getName().equals(javaType)) {
+            return "BOOLEAN";
+        }
 
-		logger.error("No sql type for java type [" + javaType + "]");
-		return null;
-	}
+        if (String.class.getName().equals(javaType)) {
+            return "VARCHAR(" + column.length() + ")";
+        }
 
-	@Override
-	public String wrap(String value) {
-		return value;
-	}
+        if (Timestamp.class.getName().equals(javaType)) {
+            return "TIMESTAMP WITH TIME ZONE";
+        }
 
-	/**
-	 * https://chartio.com/resources/tutorials/how-to-define-an-auto-increment-primary-key-in-oracle/
-	 */
-	@Override
-	public String autoIncrementType(String javaType) {
+        if (Date.class.getName().equals(javaType)) {
+            return "DATE";
+        }
 
-		if (int.class.getName().equals(javaType) || Integer.class.getName().equals(javaType)) {
-			return "SERIAL";
-		}
+        if (double.class.getName().equals(javaType) || Double.class.getName().equals(javaType)) {
+            return "DECIMAL";
+        }
 
-		if (long.class.getName().equals(javaType) || Long.class.getName().equals(javaType)) {
-			return "BIGSERIAL";
-		}
+        if (float.class.getName().equals(javaType) || Float.class.getName().equals(javaType)) {
+            return "DECIMAL";
+        }
 
-		logger.error("No sql AUTO INCREMENT type for java type [" + javaType + "]");
+        if (Json.class.getName().equals(javaType)) {
+            return "JSONB";
+        }
 
-		return null;
-	}
+        if (Xml.class.getName().equals(javaType)) {
+            return "XML";
+        }
+
+        if (Blob.class.getName().equals(javaType) || "byte[]".equals(javaType)) {
+            return "BLOB";
+        }
+
+        if (Clob.class.getName().equals(javaType)) {
+            return "CLOB";
+        }
+
+        if ("long[]".equals(javaType) || "java.lang.Long[]".equals(javaType)) {
+            return "int8[]";
+        }
+
+        if ("int[]".equals(javaType) || "java.lang.Integer[]".equals(javaType)) {
+            return "int4[]";
+        }
+
+        logger.error("No sql type for java type [" + javaType + "]");
+        return null;
+    }
+
+    @Override
+    public String wrap(String value) {
+        return value;
+    }
+
+    /**
+     * https://chartio.com/resources/tutorials/how-to-define-an-auto-increment-primary-key-in-oracle/
+     */
+    @Override
+    public String autoIncrementType(String javaType) {
+
+        if (int.class.getName().equals(javaType) || Integer.class.getName().equals(javaType)) {
+            return "SERIAL";
+        }
+
+        if (long.class.getName().equals(javaType) || Long.class.getName().equals(javaType)) {
+            return "BIGSERIAL";
+        }
+
+        logger.error("No sql AUTO INCREMENT type for java type [" + javaType + "]");
+
+        return null;
+    }
 }
